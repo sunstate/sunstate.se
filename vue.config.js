f24198e6a92edc8602671353590abc5ceba69af9
Simon Wikstrand <simon@sunstate.se>
2018-12-23 11:21:47 +0100

    Added loader to import svg as vue components

--- vue.config.js
+++ vue.config.js
@@ -19,5 +19,14 @@ module.exports = {
         views: path.resolve(__dirname, "src/views")
       }
     }
+  },
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
   }
 };
